3c0309ff3e2674f241f96bfefc4b4c58878daf5e
Hack Reactor Students <accounts@hackreactor.com>
2015-06-11 10:47:00 -0700

    Called next() in Link.pre('save'). Removed console.logs

--- app/models/link.js
+++ app/models/link.js
@@ -12,40 +12,12 @@ var Links = mongoose.Schema({
 Links.plugin(timestamps);
 
 Links.pre('save', function(next) {
-    console.log("about to save");
     var shasum = crypto.createHash('sha1');
     shasum.update(this.url);
     this.code = shasum.digest('hex').slice(0, 5);
+    next();
 });
 
 var Link = mongoose.model('Links', Links);
 
-
 module.exports = Link;
-
-
-// var db = require('../config');
-// var crypto = require('crypto');
-
-// var Link = db.Model.extend({
-//   tableName: 'urls',
-//   hasTimestamps: true,
-//   defaults: {
-//     visits: 0
-//   },
-//   initialize: function(){
-//     this.on('creating', function(model, attrs, options){
-//       var shasum = crypto.createHash('sha1');
-//       shasum.update(model.get('url'));
-//       model.set('code', shasum.digest('hex').slice(0, 5));
-//     });
-//   }
-// });
-
-
-
-// // client.open(function(err, client){
-// //     var Link = new mongodb.Collection(client, 'links');
-// //     Link.findAndModify()
-// //     client.close();
-// // });
